6c828f0b518eaaa7550f5449c910d575cb3e00c1
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-21 12:10:14 +0100

    Posts model/controller/migrations now added to the API with reference to the user model

--- rails_backend/db/schema.rb
+++ rails_backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_104642) do
+ActiveRecord::Schema.define(version: 2020_07_21_110316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_07_20_104642) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
