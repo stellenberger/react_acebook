2b772872220339a07550b357f2111814c6255184
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-21 13:22:19 +0100

    Input form for the post functionality with post request now implemented

--- react_frontend/src/components/home/homeSections/MiddleSection.jsx
+++ react_frontend/src/components/home/homeSections/MiddleSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const MiddleSection = () => {
+  const [post, setPost] = useState({content: '', user_id: 1})
+  const url = process.env.REACT_APP_URL
+  
+  const handleChange = (e) => {
+    setPost({ ...post, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post(url + '/posts', { post: post }, { withCredentials: true })
+      .then(response => {
+        console.log('post successfully created', response)
+        setPost({ ...post, content: ''})
+      })
+      .catch(error => {
+        console.log('error processing post request',error)
+      })
+  }
   return (
     <div className='home-middle-section'>
-      I am the middle section
+      I am the middle section and you will see all the posts here
+      <form onSubmit={handleSubmit}>
+        <label>Write a post</label>
+        <input type="textbox" name="content" value={post.content} onChange={handleChange}/>
+        <button type='submit'>Post</button>
+      </form>
     </div>
   )
 }
